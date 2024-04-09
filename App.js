119594c3b1afec91f6175f04d56e0a9eb7734794
RitheshBK <rithesh6nov1989@gmail.com>
2024-04-09 14:19:24 +0530

    Updated the React Code

---
+++ App.js
@@ -0,0 +1,106 @@
+/**
+ * <div class="main-parent">
+    * <div id="parent">
+    *   <div id="child">
+    *      <h1>I'm h1 tag</h1>
+    *      <h1>this is another h1 tag</h1>
+    *   </div>
+    *   <div id="child2">
+    *      <h1>I'm h1 tag</h1>
+    *      <h1>this is another h1 tag</h1>
+    *   </div>
+    * </div>
+    * <div id="parent2">
+    *   <div id="child">
+    *      <h1>I'm h1 tag</h1>
+    *      <h1>this is another h1 tag</h1>
+    *   </div>
+    *   <div id="child2">
+    *      <h1>I'm h1 tag</h1>
+    *      <h1>this is another h1 tag</h1>
+    *   </div>
+    * </div>
+ * </div>
+ * 
+ * 
+ * ReactElement(Object) => HTML(Browser understands)
+ * 
+ */
+
+const parent = React.createElement(
+   "div",
+   {class: "main-parent"},
+    [
+        React.createElement(
+            "div",
+            {id: "parent"},
+            React.createElement(
+                "div",
+                {id: "child"},
+                [
+                    React.createElement(
+                        "h1",
+                        {},
+                        "I'm h1 tag"
+                    ),
+                    React.createElement(
+                        "h1",
+                        {},
+                        "this is another h1 tag"
+                    ),
+                ]
+            )
+        ),
+        React.createElement(
+            "div",
+            {id: "parent2"},
+            React.createElement(
+                "div",
+                {id: "child"},
+                [
+                    React.createElement(
+                        "h1",
+                        {},
+                        "I'm h1 tag"
+                    ),
+                    React.createElement(
+                        "h1",
+                        {},
+                        "this is another h1 tag"
+                    ),
+                ]
+            )
+        ),
+        React.createElement(
+            "div",
+            {id: "parent3"},
+            React.createElement(
+                "div",
+                {id: "child"},
+                [
+                    React.createElement(
+                        "h1",
+                        {},
+                        "I'm h1 tag"
+                    ),
+                    React.createElement(
+                        "h1",
+                        {},
+                        "this is another h1 tag"
+                    ),
+                ]
+            )
+        )
+
+    ]
+);
+console.log(parent);
+
+const heading = React.createElement(
+    "h1",
+    {id: "heading", class: "heading-class", attr:"ddddd"}, 
+    "This is hello world from React !"
+);
+// console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
